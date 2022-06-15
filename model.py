ef13c7d38299120b6ad0a79c06acd90481e55ebc
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-15 18:58:24 -0500

    Update model.py

--- model.py
+++ model.py
@@ -544,7 +544,7 @@ class ProtBertClassifier(pl.LightningModule):
                 "params": self.model.parameters(), 
             },
         ]
-        optimizer = AdaFactor(parameters, lr=self.hparam.learning_rate)
+        optimizer = Adafactor(parameters, lr=self.hparam.learning_rate)
         total_training_steps = len(self.train_dataloader()) * self.hparam.max_epochs
         warmup_steps = total_training_steps // self.hparam.warm_up_split
         scheduler = get_linear_schedule_with_warmup(
