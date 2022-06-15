47aa7cda60cb3c64d3da37cfd8ad3ccc3efc8ada
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-14 19:54:41 -0500

    Update explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -26,10 +26,11 @@ class DataParser(object):
     
     def select_columns(self, col_names: List[str]=["PfCRT Isoform", "Amino Acid Sequence", 
                                                                "PPQ Resistance", "CQ Resistance", "Fitness"], drop_duplicate_on="Amino Acid Sequence"):
-        if drop_duplicate_on != None:
-            return self.data.loc[:,col_names].drop_duplicates(drop_duplicate_on)
-        else:
+        if drop_duplicate_on == None:
             return self.data.loc[:,col_names]
+        elif drop_duplicate_on != None:
+            return self.data.loc[:,col_names].drop_duplicates(drop_duplicate_on)
+
     
 if __name__ == "__main__":
     parser = DataParser(filename="pfcrt.csv")
