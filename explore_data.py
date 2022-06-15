908e122474e2057e80ea60420fbb7fd8f0f0d4ac
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-14 19:15:43 -0500

    Update explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import numpy as np
 import torch
+import attrs
 
+@attrs.define
 class DataParser(object):
+    def __init__(self, filename:str = None)
+        self.data = self.read_file(filename)
+        
+    @classmethod
+    def get_data(cls, filename):
+        return cls(filename)
+        
+    @staticmethod
     def read_file(filename: str):
         data = pd.read_xlsx(f"{filename}")
         return data
+    
+
+    
+    
+    
 
 
