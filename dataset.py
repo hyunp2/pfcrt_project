a74e8a364c9f716106221716c00f92aec867f0e7
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-15 19:20:34 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -40,25 +40,6 @@ class SequenceDataset(torch.utils.data.Dataset):
         input_reformats = {'input_ids': input_ids, 'token_type_ids': token_type_ids, 'attention_mask': attention_mask}
         target_reformats = {"labels": self.targets[idx]}
         return input_reformats, target_reformats
-
-class CustomSequenceDataset(torch.utils.data.Dataset):
-    """Protein sequence dataset"""
-    def __init__(self, inputs: Dict[str, torch.Tensor], targets: torch.Tensor) -> torch.utils.data.Dataset:
-        super().__init__()
-        self.inputs = inputs
-        self.targets = targets
-    
-    def __len__(self):
-        return len(self.targets) #train length...
-
-    def __getitem__(self, idx):
-        input_ids = self.inputs["input_ids"][idx] #B, L
-        token_type_ids = self.inputs["token_type_ids"][idx] #B, L
-        attention_mask = self.inputs["attention_mask"][idx] #B, L
-        input_reformats = {'input_ids': input_ids, 'token_type_ids': token_type_ids, 'attention_mask': attention_mask}
-        target_reformats = {"labels": self.targets[idx]}
-        return input_reformats, target_reformats
-    
     
 class NERSequenceDataset(torch.utils.data.Dataset):
     """Protein sequence dataset
@@ -95,7 +76,7 @@ class NERSequenceDataset(torch.utils.data.Dataset):
 if __name__ == "__main__":
     from train import get_args
     hparam = get_args()
-    dataset = load_dataset("yarongef/human_proteome_triplets", cache_dir=hparam.load_data_directory)
+#     dataset = load_dataset("yarongef/human_proteome_triplets", cache_dir=hparam.load_data_directory)
     tokenizer=BertTokenizer.from_pretrained("Rostlab/prot_bert",do_lower_case=False, return_tensors="pt",cache_dir=hparam.load_model_directory)
     
 #     stage="train"
@@ -121,4 +102,6 @@ if __name__ == "__main__":
     
     targets = data_trunc.iloc[:,2:].values #list type including nans; (B,3)
     targets = torch.from_numpy(targets).view(len(targets), -1) #target is originally list -> change to Tensor (B,1)
-        
+    
+    dataset = SequenceDataset(inputs=inputs, targets=targets)
+    dataloader = torch.utils.data.DataLoader(dataset, shuffle=True, batch_size=4)
