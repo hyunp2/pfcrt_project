fa61c1de3aa50278d67c540e51b0096e005d783d
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-04 17:17:55 -0500

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -1,1 +1,36 @@
+import torch
+import pytorch_lightning as pl
+import transformers
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+from bertviz.bertviz import head_view
+from transformers import BertTokenizer, BertModel, AutoModel, AutoTokenizer, BertConfig, BertForSequenceClassification, get_linear_schedule_with_warmup, Adafactor, AdamW
+import re, os, tqdm, requests
+from datasets import load_dataset
+import torch.nn as nn
+import logging
+import torchmetrics
+import wandb 
+from collections import OrderedDict
+from torch import optim
+from torch.utils.data import DataLoader
+import dataset as dl
+import argparse
+from typing import *
+from torchcrf import CRF
+# import BertNER
+# import BertNERTokenizer
+from model import ProtBertClassifier
 
+#https://github.com/HelloJocelynLu/t5chem/blob/main/t5chem/archived/MultiTask.py for more info
+
+# classifier.bert.pooler.dense.weight.requires_grad
+# classifier.bert.pooler.dense.training
+# classifier.classifier.weight.requires_grad
+logging.basicConfig()
+logger = logging.getLogger("BERT Fine-tuning")
+logger.setLevel(logging.DEBUG)
+
+class ProtBertClassifier(ProtBertClassifier):
+    
