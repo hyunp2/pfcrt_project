34af27bd4f955152a6cf804d7c75414db760df50
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-05 16:15:23 -0500

    Update explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -6,6 +6,7 @@ import attrs, dataclasses
 from typing import *
 import os, sys, shutil, subprocess, pathlib
 # from train import get_args
+import argparse
 import model as Model
 import finetune as FModel
 from transformers import BertTokenizer, BertModel, AutoModel, AutoTokenizer, BertConfig, BertForSequenceClassification, get_linear_schedule_with_warmup, Adafactor, AdamW
