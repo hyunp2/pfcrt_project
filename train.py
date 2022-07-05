85c297451ed3c37c43d6ded99e8ca882f40c7ddc
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-04 21:09:14 -0500

    Update train.py

--- train.py
+++ train.py
@@ -68,6 +68,8 @@ def get_args():
     parser.add_argument('--ner', '-ner', type=bool, default=False, help='NER training')
     parser.add_argument('--ner-config', '-nc', type=str, default=None, help='NER config')
     parser.add_argument('--fillna-val', '-fv', type=int, default=100, help='Dataset ignore index')
+    parser.add_argument('--train_frac', type=float, default=0.8, help='data split')
+
 
     args = parser.parse_args()
     return args
