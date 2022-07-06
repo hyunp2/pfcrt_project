ed3ff4e04ca20dd8738300e5e711b97a77a8d3e1
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-06 14:03:03 -0500

    Update focal_loss.py

--- focal_loss.py
+++ focal_loss.py
@@ -9,13 +9,16 @@ from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
     """SOFTMAX (only best class contrib) (not SIGMOID; adding all class contribs)"""
-    def __init__(self, gamma=2., alpha=None, size_average=True, threshold=100):
+    def __init__(self, gamma=2., beta: float=0.9999, size_average=True, threshold=100, weight: torch.Tensor=torch.ones(3)):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
-        self.alpha = alpha
+#         self.alpha = alpha
+        self.beta = beta
+        self.weights = weights
+        self.effective_num = torch.Tensor(list(map(lambda inp: (1 - beta) / (1 - beta**(inp)), weight ))) #nclass,
         self.threshold = threshold
-        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+#         if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
+#         if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
         self.size_average = size_average
 
     def forward(self, input, target):
@@ -25,21 +28,22 @@ class FocalLoss(nn.Module):
 #             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
             input = input #B,dim
         target = target.view(-1,1) #B,1
-        mask = (target < self.threshold).view(-1)
-        target = target[mask] #b,1
+#         mask = (target < self.threshold).view(-1)
+#         target = target[mask] #b,1
         
-        logpt = F.log_softmax(input[mask])
+        logpt = F.log_softmax(input)
         logpt = logpt.gather(1,target)
         logpt = logpt.view(-1)
         pt = logpt.data.exp() #non-diff
 
-        if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
+        if self.effective_num is not None:
+#             if self.alpha.type()!=input.data.type():
+#                 self.alpha = self.alpha.type_as(input.data)
+            self.effective_num = self.effective_num.to(logpt)
+            at = self.effective_num.gather(0,target.data.view(-1))
+            logpt = logpt * at.data
 
         loss = -1 * (1-pt)**self.gamma * logpt
-        loss = loss if mask.any() else 0. #b,
+        loss = loss #if mask.any() else 0. #b,
         if self.size_average: return loss.mean()
         else: return loss.sum()
