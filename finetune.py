0890142ef44bd4a2072c3a0061178c1b140fcc61
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-06 18:16:39 -0500

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -595,7 +595,7 @@ class ProtBertClassifier(ProtBertClassifier):
         token_type_ids = inputs["token_type_ids"].to(self.device)
         attention_mask = inputs["attention_mask"].to(self.device)
         self.forward(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask) #Called only once after loading ckpt!
-        ext = inputs.fhook["encoded_feats"] #B,dim
+        ext = self.fhook["encoded_feats"] #B,dim
         
         from imblearn.combine import SMOTEENN, SMOTETomek
         from imblearn.over_sampling import RandomOverSampler
