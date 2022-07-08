6df4a0533e0e4184691a35ec6096eff0fb6af9c8
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-07 22:23:23 -0500

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -158,7 +158,7 @@ class ProtBertClassifier(ProtBertClassifier):
  
     def __build_loss(self):
         """ Initializes the loss function/s. """
-        global loss_fn #fixes: AttributeError: Can't pickle local object 'ProtBertClassifier.__build_loss.<locals>.loss_fn'
+#         global loss_fn #fixes: AttributeError: Can't pickle local object 'ProtBertClassifier.__build_loss.<locals>.loss_fn'
         def loss_fn(predictions: dict, targets: dict, hparam: argparse.ArgumentParser, *weight_args):
             logits0 = predictions.get("logits0", 0)
             logits1 = predictions.get("logits1", 0)
