31a52de6d693bf7105148f1dd3562213180bead4
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-19 11:46:42 -0600

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -46,6 +46,8 @@ warnings.simplefilter("ignore")
 class ProtBertClassifierFinetune(L.LightningModule):
     def __init__(self, hparam: argparse.ArgumentParser) -> None:
         super(ProtBertClassifierFinetune, self).__init__()
+        self.pretrained = ProtBertClassifier(hparam=hparam)
+        
         parser = DataParser.get_data("pfcrt.csv")
         data = parser.data
         data_trunc = parser.select_columns(fill_na=self.hparam.fillna_val)
