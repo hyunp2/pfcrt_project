8a3f9cb47cfc84d02ea4ac87c6ca24e9649f412a
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-30 00:18:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -32,30 +32,30 @@ def get_args():
     parser = argparse.ArgumentParser(description='Training')
 
     #Model related
-    parser.add_argument('--load-model-directory', "-dirm", type=str, default="output", help='This is where model is/will be located...')  
-    parser.add_argument('--load-model-checkpoint', "-ckpt", type=str, default=None, help='which checkpoint...')  
-    parser.add_argument('--artifact-checkpoint', "-ackpt", type=str, default=None,  
+    parser.add_argument('--load_model_directory', "-dirm", type=str, default="output", help='This is where model is/will be located...')  
+    parser.add_argument('--load_model_checkpoint', "-ckpt", type=str, default=None, help='which checkpoint...')  
+    parser.add_argument('--artifact_checkpoint', "-ackpt", type=str, default=None,  
                         help='name to save/load atfifact checkpoint..., this is the name of SAVED model (e.g. epoch=50-val_loss=20.ckpt) py PL')  
-    parser.add_argument('--model-name', type=str, default='Rostlab/prot_bert', help='HUGGINGFACE Backbone model name card')
+    parser.add_argument('--model_name', type=str, default='Rostlab/prot_bert', help='HUGGINGFACE Backbone model name card')
     parser.add_argument('--finetune', action="store_true")
 
     #Molecule (Dataloader) related
-    parser.add_argument('--load-data-directory', "-dird", default="data", help='This is where data is located...')  
+    parser.add_argument('--load_data_directory', "-dird", default="data", help='This is where data is located...')  
     parser.add_argument('--dataset', type=str, default="yarongef/human_proteome_triplets", help='pass dataset...')  
 
     #Optimizer related
     parser.add_argument('--optimizer', default="adamw", type=str, help='optimizer')
-    parser.add_argument('--max-epochs', default=60, type=int, help='number of epochs max')
-    parser.add_argument('--min-epochs', default=1, type=int, help='number of epochs min')
-    parser.add_argument('--batch-size', '-b', default=2048, type=int, help='batch size')
-    parser.add_argument('--learning-rate', '-lr', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--max_epochs', default=60, type=int, help='number of epochs max')
+    parser.add_argument('--min_epochs', default=1, type=int, help='number of epochs min')
+    parser.add_argument('--batch_size', '-b', default=2048, type=int, help='batch size')
+    parser.add_argument('--learning_rate', '-lr', default=1e-3, type=float, help='learning rate')
     parser.add_argument('--ngpus', default="auto", help='Number of GPUs, -1 use all available. Use CUDA_VISIBLE_DEVICES=1, to decide gpus')
-    parser.add_argument('--num-nodes', type=int, default=1, help='Number of nodes')
-    parser.add_argument('--warm-up-split', type=int, default=5, help='warmup times')
+    parser.add_argument('--num_nodes', type=int, default=1, help='Number of nodes')
+    parser.add_argument('--warm_up_split', type=int, default=5, help='warmup times')
     parser.add_argument('--scheduler', type=str, default="cosine", help='scheduler type')
     parser.add_argument('--accelerator', "-accl", type=str, default="gpu", help='accelerator type', choices=["cpu","gpu","tpu"])
     parser.add_argument('--strategy', "-st", default="ddp", help='accelerator type', choices=["ddp_spawn","ddp","dp","ddp2","horovod","none", "auto"]) 
-    parser.add_argument('--loss-weights', type=float, nargs="*", required=True, help='which loss to ignore during finetuing')
+    parser.add_argument('--loss_weights', type=float, nargs="*", required=True, help='which loss to ignore during finetuing')
 
     #Misc.
     parser.add_argument('--seed', type=int, default=42, help='seeding number')
@@ -65,16 +65,16 @@ def get_args():
     parser.add_argument('--save_top_k', type=int, default="5", help='num of models to save')
     parser.add_argument('--patience', type=int, default=10, help='patience for stopping')
     parser.add_argument('--metric_mode', type=str, default="min", help='mode of monitor')
-    parser.add_argument('--distributed-backend', default='ddp', help='Distributed backend: dp, ddp, ddp2')
-    parser.add_argument('--num-workers', type=int, default=4, help='Number of workers for data prefetch')
-    parser.add_argument('--amp-backend', type=str, default="native", help='Torch vs NVIDIA AMP')
+    parser.add_argument('--distributed_backend', default='ddp', help='Distributed backend: dp, ddp, ddp2')
+    parser.add_argument('--num_workers', type=int, default=4, help='Number of workers for data prefetch')
+    parser.add_argument('--amp_backend', type=str, default="native", help='Torch vs NVIDIA AMP')
     parser.add_argument('--max_length', type=int, default=1536, help='length for padding seqs')
-    parser.add_argument('--label-smoothing', '-ls', type=float, default=0., help='CE loss regularization')
-    parser.add_argument('--sanity-checks', '-sc', type=int, default=2, help='Num sanity checks..')
+    parser.add_argument('--label_smoothing', '-ls', type=float, default=0., help='CE loss regularization')
+    parser.add_argument('--sanity_checks', '-sc', type=int, default=2, help='Num sanity checks..')
     parser.add_argument('--z_dim', '-zd', type=int, default=1024, help='CURL purpose.., SAME as self.encoder_features')
     parser.add_argument('--ner', '-ner', type=bool, default=False, help='NER training')
-    parser.add_argument('--ner-config', '-nc', type=str, default=None, help='NER config')
-    parser.add_argument('--fillna-val', '-fv', type=int, default=100, help='Dataset ignore index')
+    parser.add_argument('--ner_config', '-nc', type=str, default=None, help='NER config')
+    parser.add_argument('--fillna_val', '-fv', type=int, default=100, help='Dataset ignore index')
     parser.add_argument('--train_frac', type=float, default=0.8, help='data split')
     parser.add_argument('--nonuniform_weight', action="store_true", help='Weighted CE loss')
     parser.add_argument('--use_ce', action="store_true", help='CE vs Focal loss')
@@ -153,7 +153,8 @@ def _main():
     if hparams.load_model_checkpoint and not hparams.finetune:
         resume_ckpt = os.path.join(hparams.load_model_directory, hparams.load_model_checkpoint)
     elif hparams.load_model_checkpoint and hparams.finetune:
-        resume_ckpt = None
+        # resume_ckpt = None
+        resume_ckpt = os.path.join(hparams.load_model_directory, hparams.load_model_checkpoint)
     else:
         resume_ckpt = None
         
@@ -179,7 +180,7 @@ def _main():
         accelerator=hparams.accelerator,
     )
 
-    trainer.fit(model, ckpt_path=resume_ckpt) #New API!
+    trainer.fit(model) #, ckpt_path=resume_ckpt) #New API!
 
 if __name__ == "__main__":
     _main()
@@ -190,4 +191,4 @@ if __name__ == "__main__":
     #python -m train --ngpus "auto" --accelerator gpu --strategy none -b 8 --finetune -ckpt ckpt_for_finetune.ckpt --use_ce --nonuniform_weight
 
     #[Dec 19th 2023] git pull && python -m main --ngpus "auto" --accelerator gpu --strategy none -b 512 
-    #[Dec 30th 2023] git pull && python -m main --ngpus "auto" --accelerator gpu --strategy none -b 512 --finetune --loss_weights 0 1 0
+    #[Dec 30th 2023] git pull && python -m main --ngpus "auto" --accelerator gpu --strategy none -b 512 --finetune --loss-weights 0 1 0 --load_model_directory output_finetune --load_model_checkpoint best_pretrained.ckpt
