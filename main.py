d6ad996df9fba43adda315ca04da88711519bbcb
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-30 00:08:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -190,4 +190,4 @@ if __name__ == "__main__":
     #python -m train --ngpus "auto" --accelerator gpu --strategy none -b 8 --finetune -ckpt ckpt_for_finetune.ckpt --use_ce --nonuniform_weight
 
     #[Dec 19th 2023] python -m train --ngpus "auto" --accelerator gpu --strategy none -b 512 
-    #[Dec 30th 2023] python -m train --ngpus "auto" --accelerator gpu --strategy none -b 512 --finetune 
+    #[Dec 30th 2023] python -m train --ngpus "auto" --accelerator gpu --strategy none -b 512 --finetune --loss_weights 0 1 0
