0e2c2ac3db6ed06597bd8ec2f86dbf797bc051a0
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-31 00:11:17 -0600

    Update focal_loss.py

--- focal_loss.py
+++ focal_loss.py
@@ -24,10 +24,7 @@ class FocalLoss(nn.Module):
 
     def forward(self, input, target):
         assert input.dim() == 2 or input.dim() == 3:
-#             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-#             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-#             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-            input = input #B,dim
+
         target = target.view(-1,1) #B,1
 #         mask = (target < self.threshold).view(-1)
 #         target = target[mask] #b,1
