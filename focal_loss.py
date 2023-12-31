057a77242400b11126798fd2799c49fdb5520f23
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-31 00:10:31 -0600

    Update focal_loss.py

--- focal_loss.py
+++ focal_loss.py
@@ -23,7 +23,7 @@ class FocalLoss(nn.Module):
         self.ignore_index = ignore_index
 
     def forward(self, input, target):
-        if input.dim() == 2:
+        assert input.dim() == 2 or input.dim() == 3:
 #             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
 #             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
 #             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
