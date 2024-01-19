62f448835e90518e1f914ddf2329db1b5f33077d
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-01-19 12:45:08 -0600

    Update explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -28,6 +28,7 @@ class DataParser(object):
         ext = os.path.splitext(filename)[-1]
         if ext in [".xlsx"]:
             data = pd.read_excel(f"{filename}", header=2)
+            data.iloc[:-9,:]
         elif ext in [".csv"]:
             data = pd.read_csv(f"{filename}")        
         return data
@@ -36,15 +37,15 @@ class DataParser(object):
                                                                "PPQ Resistance", "CQ Resistance", "Fitness"], drop_duplicate_on="Amino Acid Sequence", fill_na=None, dropna=True):
         if drop_duplicate_on == None:
             data = self.data.loc[:,col_names]
-            data = data.replace({"PPQ Resistance": 2.0}, 1.0) #-> convert to binary , 
-            data = data.replace({"CQ Resistance": 2.0}, 1.0)            
+            # data = data.replace({"PPQ Resistance": 2.0}, 1.0) #-> convert to binary , 
+            # data = data.replace({"CQ Resistance": 2.0}, 1.0)            
             data.fillna(value=fill_na, inplace=True)
             data.dropna(axis=0, inplace=dropna)
             return data
         elif drop_duplicate_on != None:
             data = self.data.loc[:,col_names].drop_duplicates(drop_duplicate_on)
-            data = data.replace({"PPQ Resistance": 2.0}, 1.0) #-> convert to binary , 
-            data = data.replace({"CQ Resistance": 2.0}, 1.0)
+            # data = data.replace({"PPQ Resistance": 2.0}, 1.0) #-> convert to binary , 
+            # data = data.replace({"CQ Resistance": 2.0}, 1.0)
             data.fillna(value=fill_na, inplace=True) 
             data.dropna(axis=0, inplace=dropna)
             return data
