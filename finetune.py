b7b9ca1fb1598a3e50d043f97f65074ef7b32453
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-01-20 23:41:14 -0600

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -789,10 +789,10 @@ class ProtBertClassifierFinetune(L.LightningModule):
         train, val = self._tokenize_and_split(proper_inputs, targets, isos)
         test = self._tokenize_and_split(proper_inputs, targets, ~isos, split=False)
 
-        print(len(proper_inputs), proper_inputs[slice(-9,None)])
-        print(len(isos))
-        print(len(targets))
-        print(len(train) + len(val) + len(test))
+        # print(len(proper_inputs), proper_inputs[slice(-9,None)])
+        # print(len(isos))
+        # print(len(targets))
+        # print(len(train) + len(val) + len(test))
         
         if stage == "train":
             dataset = train
